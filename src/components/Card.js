15f1ac633c82e02fbece29212920ed1024f35ab9
Bueno <134732362+gbueno10@users.noreply.github.com>
2024-03-04 12:30:03 +0000

    Update Card.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -44,7 +44,8 @@ const Card = () => {
         <div className="flex items-center">
           <LevelIndicator onLevelChange={handleLevelChange} />
           <button className="ml-4 bg-gray-800 text-white px-4 py-2 rounded" onClick={handleRandomQuestion}>
-            Sortear Nova Pergunta
+            New Card
+
           </button>
         </div>
       </div>
