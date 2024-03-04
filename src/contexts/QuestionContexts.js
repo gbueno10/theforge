3aa8f542c1551de41328d3b547f3e0902bcd8c88
Bueno <134732362+gbueno10@users.noreply.github.com>
2024-03-04 12:28:06 +0000

    Update QuestionContexts.js

--- src/contexts/QuestionContexts.js
+++ src/contexts/QuestionContexts.js
@@ -11,42 +11,133 @@ export const useQuestionContext = () => {
 export const QuestionProvider = ({ children }) => {
   const initialQuestions = [
     [
-      { question: "Se você pudesse escolher qualquer pessoa do mundo, quem você iria querer como convidado para um jantar?", level: 1 },
-      { question: "Você gostaria de ser famoso? De que forma?", level: 1 },
-      { question: "Antes de fazer um telefonema, você costuma ensaiar o que vai dizer? Por quê?", level: 1 },
-      { question: "O que constitui um dia “perfeito” para você?", level: 2 },
-      { question: "Quando foi a última vez que você cantou sozinho? E para outra pessoa?", level: 1 },
-      { question: "Se você viver até os 90 anos e se pudesse escolher entre manter o corpo ou a mente dos seus 30 pelos últimos 60 anos de sua vida, qual você escolheria?", level: 2 },
-      { question: "Você tem algum pressentimento sobre como você vai morrer?", level: 3 },
-      { question: "Cite três coisas que você e seu (sua) parceiro(a) parecem ter em comum.", level: 1 },
-      { question: "Pelo que você se sente mais grato em sua vida?", level: 2 },
-      { question: "Se você pudesse mudar algo sobre a forma de como foi criado, o que seria?", level: 2 },
-      { question: "Conte a seu (sua) parceiro(a), em quatro minutos, a história de sua vida da maneira mais detalhada possível.", level: 3 },
-      { question: "Se você pudesse acordar amanhã com uma qualidade ou habilidade nova, qual seria?", level: 2 },
-    
-      { question: "Se uma bola de cristal pudesse revelar a verdade sobre você mesmo, sua vida, seu futuro ou qualquer outra coisa, o que você gostaria de saber?", level: 3 },
-      { question: "Há algo que você sonha em fazer há muito tempo? Por que ainda não o fez?", level: 2 },
-      { question: "Qual é a maior realização da sua vida?", level: 2 },
-      { question: "O que você mais valoriza em uma amizade?", level: 2 },
-      { question: "Qual é a sua lembrança mais preciosa?", level: 2 },
-      { question: "Qual é a sua lembrança mais terrível?", level: 3 },
-      { question: "Se você soubesse que em um ano você irá morrer subitamente, você mudaria alguma coisa sobre a forma de como está vivendo agora? Por quê?", level: 3 },
-      { question: "O que a amizade significa para você?", level: 1 },
-      { question: "Alternando as respostas, diga cinco coisas que você considera características positivas do seu (sua) parceiro(a). Mencione um total de cinco características.", level: 1 },
-      { question: "Como você se sente sobre o relacionamento com a sua mãe?", level: 2 },
-    
-      { question: "Complete a frase: 'Eu gostaria de ter alguém com quem eu pudesse dividir...'", level: 2 },
-      { question: "Se você fosse se tornar um amigo próximo de seu (sua) parceiro(a), compartilhe o que seria importante que ele ou ela soubesse.", level: 1 },
-      { question: "Diga a seu (sua) parceiro(a) o que você gosta nele; seja bem honesto desta vez, dizendo coisas que você não diria a alguém que tivesse acabado de conhecer.", level: 1 },
-      { question: "Compartilhe com seu (sua) parceiro(a) um momento embaraçoso de sua vida.", level: 3 },
-      { question: "Quando foi a última vez que você chorou na frente de outra pessoa? E sozinho?", level: 2 },
-      { question: "Diga a seu (sua) parceiro(a) algo que você já gosta nele(a).", level: 1 },
-      { question: "Se você fosse morrer esta noite sem ter a oportunidade de se comunicar com ninguém, do que você se arrependeria de não ter dito? Por que você ainda não disse isso a essa pessoa?", level: 3 },
-      { question: "Sua casa, com tudo o que você possui, pega fogo. Depois de salvar todas as pessoas e animais, você tem tempo para uma volta final, com segurança, para salvar qualquer outro objeto. O que seria? Por quê?", level: 1 },
-      { question: "De todas as pessoas de sua família, a morte de quem você consideraria mais arrasadora?", level: 3 },
-
-
-    ],
+      
+    {
+        "question": "If you could choose anyone in the world, who would you want as a guest for dinner?",
+        "level": 1
+    },
+    {
+        "question": "Would you like to be famous? How?",
+        "level": 1
+    },
+    {
+        "question": "Before making a phone call, do you usually rehearse what you're going to say? Why?",
+        "level": 1
+    },
+    {
+        "question": "What makes a 'perfect' day for you?",
+        "level": 2
+    },
+    {
+        "question": "When was the last time you sang alone? And to someone else?",
+        "level": 1
+    },
+    {
+        "question": "If you were to live to 90 years old and could choose between keeping your body or mind at 30 for the last 60 years of your life, which would you choose?",
+        "level": 2
+    },
+    {
+        "question": "Do you have any premonition about how you will die?",
+        "level": 3
+    },
+    {
+        "question": "Name three things you and your partner seem to have in common.",
+        "level": 1
+    },
+    {
+        "question": "What are you most grateful for in your life?",
+        "level": 2
+    },
+    {
+        "question": "If you could change something about the way you were raised, what would it be?",
+        "level": 2
+    },
+    {
+        "question": "Tell your partner, in four minutes, the story of your life in as much detail as possible.",
+        "level": 3
+    },
+    {
+        "question": "If you could wake up tomorrow with a new quality or skill, what would it be?",
+        "level": 2
+    },
+    {
+        "question": "If a crystal ball could reveal the truth about yourself, your life, your future, or anything else, what would you want to know?",
+        "level": 3
+    },
+    {
+        "question": "Is there something you've been dreaming of doing for a long time? Why haven't you done it yet?",
+        "level": 2
+    },
+    {
+        "question": "What is the greatest accomplishment of your life?",
+        "level": 2
+    },
+    {
+        "question": "What do you value most in a friendship?",
+        "level": 2
+    },
+    {
+        "question": "What is your most precious memory?",
+        "level": 2
+    },
+    {
+        "question": "What is your most terrible memory?",
+        "level": 3
+    },
+    {
+        "question": "If you knew you would die suddenly in a year, would you change anything about how you're living now? Why?",
+        "level": 3
+    },
+    {
+        "question": "What does friendship mean to you?",
+        "level": 1
+    },
+    {
+        "question": "Alternating answers, name five positive qualities of your partner. Mention a total of five characteristics.",
+        "level": 1
+    },
+    {
+        "question": "How do you feel about your relationship with your mother?",
+        "level": 2
+    },
+    {
+        "question": "Complete the sentence: 'I wish I had someone with whom I could share...'",
+        "level": 2
+    },
+    {
+        "question": "If you were to become close friends with your partner, share what would be important for him or her to know.",
+        "level": 1
+    },
+    {
+        "question": "Tell your partner what you like about them; be very honest this time, saying things you wouldn't say to someone you just met.",
+        "level": 1
+    },
+    {
+        "question": "Share with your partner an embarrassing moment from your life.",
+        "level": 3
+    },
+    {
+        "question": "When was the last time you cried in front of another person? And alone?",
+        "level": 2
+    },
+    {
+        "question": "Tell your partner something you already like about them.",
+        "level": 1
+    },
+    {
+        "question": "If you were to die tonight without the opportunity to communicate with anyone, what would you regret not having said? Why haven't you said this to that person yet?",
+        "level": 3
+    },
+    {
+        "question": "Your house, with everything you own, catches fire. After saving all people and animals, you have time for one final, safe round to save any other object. What would it be? Why?",
+        "level": 1
+    },
+    {
+        "question": "Of all the people in your family, whose death would you consider most devastating?",
+        "level": 3
+    }
+]
+,
   ];
 
   const [questions, setQuestions] = useState(initialQuestions);
